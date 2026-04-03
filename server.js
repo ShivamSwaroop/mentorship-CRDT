d20c32a727b131ad8ea2fed5da99ea784e2549e7
ShivamSwaroop <dshivamswaroop@gmail.com>
2026-04-03 22:58:57 +0530

    Deploy commit of CRDT

---
+++ server.js
@@ -0,0 +1,17 @@
+import http from "http";
+import WebSocket from "ws";
+import { setupWSConnection } from "y-websocket/bin/utils.js";
+
+const server = http.createServer();
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (conn, req) => {
+  setupWSConnection(conn, req);
+});
+
+const PORT = process.env.PORT || 1234;
+
+server.listen(PORT, () => {
+  console.log(`Yjs server running on port ${PORT}`);
+});
